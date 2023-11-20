91f8af43669e06e128fc35edaf2dd2ccab55bf29
Mikkareem <mohdirsath1997@gmail.com>
2023-11-20 20:31:44 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.6173f36d62deb653e3a62905178a9c2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612eba5fdabe817824a462e8feb353b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd47b502b497fc2ffc08",
+    "url": "/static/css/main.5d3bb32b.chunk.css"
+  },
+  {
+    "revision": "7a39133b6b5f28a8716b",
+    "url": "/static/js/2.8eeda94a.chunk.js"
+  },
+  {
+    "revision": "fd47b502b497fc2ffc08",
+    "url": "/static/js/main.8fa83292.chunk.js"
+  },
+  {
+    "revision": "f1b768174fa530eb5144",
+    "url": "/static/js/runtime-main.39dd3371.js"
+  }
+]);
